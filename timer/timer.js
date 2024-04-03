5b8e58bb089029ea766898dc12dbd7c874b8fe50
Doyeon <duseh729@naver.com>
2024-04-03 09:16:46 +0900

    fix: 타이머 요구사항에 맞게 기능 변경

--- timer/timer.js
+++ timer/timer.js
@@ -1,3 +1,7 @@
+const hourInput = document.getElementById("hour-input");
+const minuteInput = document.getElementById("minute-input");
+const secondInput = document.getElementById("second-input");
+
 const startButton = document.getElementById("start-button");
 const stopButton = document.getElementById("stop-button");
 const resetButton = document.getElementById("reset-button");
@@ -13,20 +17,37 @@ let trigger = 0;
 const timerHandler = (arg = null) => {
   // second = 0;
   if (trigger) {
-    if (second == 59) {
-      second = -1;
-      minute += 1;
-    } else if (minute == 59) {
-      minute = -1;
-      hour += 1;
+    if (second >= 1) {
+      second -= 1;
+    } else if (minute >= 1) {
+      minute -= 1;
+      second = 59;
+    } else if (hour >= 1) {
+      hour -= 1;
+      minute = 59;
+      second = 59;
     }
-    second += 1;
     timerDisplay.innerHTML = `<p>${String(hour).padStart(2, "0")}:${String(minute).padStart(2, [0])}:${String(second).padStart(2, "0")}</p>`;
+    if (hour == 0 && minute == 0 && second == 0) {
+      alert("타이머 종료");
+      trigger = 0;
+    }
     console.log(second);
   }
 };
 
 setInterval(timerHandler, 1000);
+
+hourInput.addEventListener("change", e => {
+  console.log(e.target.value);
+  hour = e.target.value;
+});
+minuteInput.addEventListener("change", e => {
+  minute = e.target.value;
+});
+secondInput.addEventListener("change", e => {
+  second = e.target.value;
+});
 
 startButton.addEventListener("click", () => {
   trigger = 1;
@@ -35,9 +56,12 @@ stopButton.addEventListener("click", () => {
   trigger = 0;
 });
 resetButton.addEventListener("click", () => {
+  trigger = 0;
   second = 0;
   minute = 0;
   hour = 0;
+  hourInput.value = null;
+  minuteInput.value = null;
+  secondInput.value = null;
   timerDisplay.innerHTML = `<p>${String(hour).padStart(2, "0")}:${String(minute).padStart(2, [0])}:${String(second).padStart(2, "0")}</p>`;
-  trigger = 0;
 });
