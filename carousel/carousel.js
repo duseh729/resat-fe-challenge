f961afe350dcde2746ec292abfdd98454fd32bd4
Doyeon <duseh729@naver.com>
2024-04-08 18:58:49 +0900

    feat: 캐러셀

---
+++ carousel/carousel.js
@@ -0,0 +1,31 @@
+const CAROUSEL_LENGTH = document.querySelectorAll(".carousel-item").length - 1;
+let current = 0;
+
+const carouselContainer = document.querySelector("#carousel-container");
+const prevButton = document.querySelector("#prev-button");
+const nextButton = document.querySelector("#next-button");
+
+const nextEvent = () => {
+  console.log("다음");
+  if (current !== CAROUSEL_LENGTH) {
+    carouselContainer.style.transform = `translateX(${(current + 1) * -370}px)`;
+    current++;
+  } else {
+    current = 0;
+    carouselContainer.style.transform = `translateX(0px)`;
+  }
+};
+
+const prevEvent = () => {
+  console.log("이전");
+  if (current !== 0) {
+    current--;
+    carouselContainer.style.transform = `translateX(${current * -370}px)`;
+  } else {
+    current = CAROUSEL_LENGTH;
+    carouselContainer.style.transform = `translateX(${CAROUSEL_LENGTH * -370}px)`;
+  }
+};
+
+nextButton.addEventListener("click", nextEvent);
+prevButton.addEventListener("click", prevEvent);
